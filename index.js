8a681563d988406c9a8c04ab632dfdd62989c6a8
Bafokeng Ramalitse <drramalitse@gmail.com>
2024-10-09 01:32:16 +0800

    Coding....

---
+++ index.js
@@ -0,0 +1,5 @@
+import WebSocket from "ws";
+import dotenv from "dotenv";
+import fastify from "fastify";
+import fastifyFormbody from "@fastify/formbody";
+import fastifyWs from "@fastify/websocket";
